70786acf50534995c0cb0448ff7e9621532ada7e
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2023-02-09 12:00:58 +0000

    sql

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
+  host="localhost",  #locazione
+  user="root",    
   password=""
 )
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor()  #crea un cursore su phyton che scrive nel cursore di sql
 
-mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("CREATE DATABASE mydatabase")  #scrive al nostro posto in sql, grazie al cursone
